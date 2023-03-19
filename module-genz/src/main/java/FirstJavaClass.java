150f6a39fef0585f4de42debbecca803ea3e97a7
Akash <akash.patil@genzsoln.com>
2023-03-19 21:21:02 +0530

    Second Commit

--- module-genz/src/main/java/FirstJavaClass.java
+++ module-genz/src/main/java/FirstJavaClass.java
@@ -3,5 +3,6 @@ package main.java;
 public class FirstJavaClass {
     public static void main(String[] args) {
         System.out.println("First push on git");
+        System.out.println("second push on git");
     }
 }
